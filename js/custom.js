e42c7c20baa124618bf1b22d4bb34079bb910342
activhauling <activtruckin@gmail.com>
2026-01-21 12:41:04 -0800

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,31 @@
-// to get current year
+// ==============================
+// Display current year in footer
+// ==============================
 function getYear() {
     var currentDate = new Date();
     var currentYear = currentDate.getFullYear();
-    document.querySelector("#displayYear").innerHTML = currentYear;
+    var yearEl = document.querySelector("#displayYear");
+    if (yearEl) {
+        yearEl.innerHTML = currentYear;
+    }
 }
 
 getYear();
 
 
-//  owl carousel script
-$(".owl-carousel").owlCarousel({
+// ==============================
+// Generic Owl Carousel
+// (EXCLUDES testimonials)
+// ==============================
+$(".owl-carousel").not(".testimonial-carousel").owlCarousel({
     loop: true,
     margin: 20,
     nav: true,
     autoplay: true,
-    navText: ['<i class="fa fa-long-arrow-left" aria-hidden="true"></i>', '<i class="fa fa-long-arrow-right" aria-hidden="true"></i>'],
+    navText: [
+        '<i class="fa fa-long-arrow-left" aria-hidden="true"></i>',
+        '<i class="fa fa-long-arrow-right" aria-hidden="true"></i>'
+    ],
     autoplayHoverPause: true,
     responsive: {
         0: {
@@ -26,17 +37,18 @@ $(".owl-carousel").owlCarousel({
     }
 });
 
-//    end owl carousel script 
 
-// NEW testimonial carousel(add) 
+// ==============================
+// Continuous Testimonial Carousel
+// ==============================
 $(".testimonial-carousel").owlCarousel({
     loop: true,
     margin: 25,
     autoplay: true,
-    autoplayTimeout: 0,
-    autoplaySpeed: 6000,
+    autoplayTimeout: 0,          // no pauses
+    autoplaySpeed: 6000,         // constant speed
     smartSpeed: 6000,
-    slideTransition: 'linear',
+    slideTransition: "linear",   // smooth continuous motion
     dots: false,
     nav: false,
     mouseDrag: false,
@@ -44,19 +56,30 @@ $(".testimonial-carousel").owlCarousel({
     pullDrag: false,
     freeDrag: false,
     responsive: {
-        0: { items: 1 },
-        768: { items: 2 },
-        1000: { items: 3 }
+        0: {
+            items: 1
+        },
+        768: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
     }
 });
 
-/** google_map js **/
+
+// ==============================
+// Google Map
+// ==============================
 function myMap() {
     var mapProp = {
         center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
+        zoom: 18
     };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 
+    var mapEl = document.getElementById("googleMap");
+    if (mapEl) {
+        new google.maps.Map(mapEl, mapProp);
+    }
 }
-
